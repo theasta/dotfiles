9cf9595decd9137032cd87df9e20da33417a2074
Alexandrine Boissière <theasta@gmail.com>
2013-03-24 20:21:41 -0700

    Don't copy bootstrap.sh into the home dir

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "readme.md" --exclude "install-deps.sh" --exclude ".osx" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "readme.md" --exclude "bootstrap.sh" --exclude ".osx" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
