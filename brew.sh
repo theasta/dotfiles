bcd12ef5c6176ddb52e31f3f8fe278115569d6d0
Alexandrine Boissière <theasta@gmail.com>
2022-04-29 18:28:03 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -24,7 +24,7 @@ brew install bash
 
 brew install bash-completion
 
-brew install homebrew/completions/brew-cask-completion
+brew install grc
 
 # Install wget with IRI support
 brew install wget --with-iri
@@ -39,43 +39,17 @@ brew install homebrew/dupes/screen
 # z hopping around folders
 brew install z
 
-# run this script when this file changes guy.
-# brew install entr
-
-# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
-brew install hub
-
-
-# mtr - ping & traceroute. best.
-# brew install mtr
-
-    # allow mtr to run without sudo
-    # mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
-    # sudo chmod 4755 $mtrlocation/sbin/mtr
-    # sudo chown root $mtrlocation/sbin/mtr
-
-
 # Install other useful binaries
 brew install the_silver_searcher
-brew install fzf
 
 brew install git
 brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
-# brew install pv
-# brew install rename
+brew install node
+
 brew install tree
-# brew install zopfli
+
 brew install ffmpeg --with-libvpx
 
-brew install terminal-notifier
-
-# brew install android-platform-tools
-# brew install pidcat   # colored logcat guy
-
-# brew install ncdu # find where your diskspace went
-
-# brew install zsh
 
 # Remove outdated versions from the cellar
 brew cleanup
